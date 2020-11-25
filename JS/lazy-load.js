592ffafce6597d05b2e412dad06457fee83b6709
Maksym Zyza <mmziza88@gmail.com>
2020-11-25 15:31:14 +0200

    Додано приклад 2

--- JS/lazy-load.js
+++ JS/lazy-load.js
@@ -24,4 +24,64 @@ const io = new IntersectionObserver(onEntry, options);
 
 images.forEach((image) => io.observe(image));
 
-// Один обзервер желательно использовать для колекции однотипних елементов
+// Один Observer желательно использовать для колекции однотипних елементов
+
+// Пример через ф-ю и второй додход (на каждий ел-т свой Observer):
+
+// ===========================
+// const lazyLoad = targets => {
+//   const options = {
+//     rootMargin: '100px',
+//   };
+
+//   const onEntry = (entries, observer) => {
+//     console.log('🐷');
+
+//     entries.forEach(entry => {
+//       if (entry.isIntersecting) {
+//         const image = entry.target;
+//         const src = image.dataset.lazy;
+
+//         image.src = src;
+//         image.classList.add('appear');
+
+//         observer.unobserve(image);
+//       }
+//     });
+//   };
+
+//   const io = new IntersectionObserver(onEntry, options);
+
+//   targets.forEach(target => io.observe(target));
+// };
+
+// lazyLoad(images);
+
+/*
+ * Intersection Observer с disconnect (на каждый свой)
+ */
+// const images = document.querySelectorAll('.feed img');
+
+// const lazyLoad = target => {
+//   const options = {
+//     rootMargin: '100px',
+//   };
+
+//   const io = new IntersectionObserver((entries, observer) => {
+//     entries.forEach(entry => {
+//       if (entry.isIntersecting) {
+//         const image = entry.target;
+//         const src = image.dataset.lazy;
+
+//         image.src = src;
+//         image.classList.add('appear');
+
+//         observer.disconnect();
+//       }
+//     });
+//   }, options);
+
+//   io.observe(target);
+// };
+
+// images.forEach(image => lazyLoad(image));
